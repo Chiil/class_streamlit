802bba2c80dc0328e5e23d1fa2139f6e2fc2a824
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2025-05-27 23:53:59 +0200

    Updating is not going well yet, but basic functionality is there.

--- class_main.py
+++ class_main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import copy
 import numpy as np
 import pandas as pd
 import tomllib
@@ -38,6 +39,8 @@ class MixedLayerModel:
         self.wtheta = settings["wtheta"]
         self.gammatheta = settings["gammatheta"]
 
+        self.run()
+
 
     def step(self):
         # First, compute the growth.
@@ -62,6 +65,7 @@ class MixedLayerModel:
 
     def run(self):
         self.time = 0
+
         nt = round(self.runtime / self.dt)
         nt_output = round(nt * self.dt / self.dt_output)
         nt_ratio = round(self.dt_output / self.dt)
@@ -126,7 +130,7 @@ with st.sidebar:
     clone_run, edit_run, delete_run = st.columns(3)
     if clone_run.button("", icon=":material/content_copy:", use_container_width=True):
         cloned_run = st.session_state.all_runs_key + " (clone)"
-        st.session_state.all_runs[cloned_run] = MixedLayerModel(default_settings)
+        st.session_state.all_runs[cloned_run] = MixedLayerModel(st.session_state.all_runs[st.session_state.all_runs_key].settings)
         st.session_state.all_runs_key = cloned_run
         st.rerun()
     if edit_run.button("", icon=":material/edit:", use_container_width=True):
@@ -148,18 +152,20 @@ if st.session_state.main_mode == 0:
     col_plot1, col_plot2 = st.columns(2)
 
     with col_plot1.container(border=True):
-        h = default.output["h"].values[-1]
-        theta = default.output["theta"].values[-1]
-        dtheta = default.output["dtheta"].values[-1]
-        gammatheta = default.gammatheta
-
-        z_plot = np.array([ 0, h, h, 2000.0 ])
-        theta_plot = np.array([ theta, theta, theta + dtheta, theta + dtheta + gammatheta*(2000.0-h) ])
-
-        df = pd.DataFrame({"theta": theta_plot, "z": z_plot })
-
         fig = go.Figure()
-        fig.add_trace(go.Scatter(x=df["theta"], y=df["z"], mode="lines+markers", name="last"))
+
+        for name, run in st.session_state.all_runs.items():
+            h = run.output["h"].values[-1]
+            theta = run.output["theta"].values[-1]
+            dtheta = run.output["dtheta"].values[-1]
+            gammatheta = run.gammatheta
+
+            z_plot = np.array([ 0, h, h, 2000.0 ])
+            theta_plot = np.array([ theta, theta, theta + dtheta, theta + dtheta + gammatheta*(2000.0-h) ])
+
+            df = pd.DataFrame({"theta": theta_plot, "z": z_plot })
+
+            fig.add_trace(go.Scatter(x=df["theta"], y=df["z"], mode="lines+markers", name=name))
 
         fig.update_layout(margin={'t': 50, 'l': 0, 'b': 0, 'r': 0}, xaxis_title="theta (K)", yaxis_title="z (m)")
         st.plotly_chart(fig)
@@ -167,7 +173,8 @@ if st.session_state.main_mode == 0:
 
     with col_plot2.container(border=True):
         fig = go.Figure()
-        fig.add_trace(go.Scatter(x=default.output.index / 3600, y=default.output["h"], mode="lines+markers", name="run 1"))
+        for name, run in st.session_state.all_runs.items():
+            fig.add_trace(go.Scatter(x=run.output.index / 3600, y=run.output["h"], mode="lines+markers", name=name))
         fig.update_layout(margin={'t': 50, 'l': 0, 'b': 0, 'r': 0}, xaxis_title="time (h)", yaxis_title="h (m)")
         st.plotly_chart(fig)
 
@@ -175,7 +182,13 @@ if st.session_state.main_mode == 0:
 elif st.session_state.main_mode == 1:
     st.header("Edit run")
 
-    col1, col2 = st.columns(2, vertical_alignment="bottom")
+    active_run = st.session_state.all_runs[st.session_state.all_runs_key]
+    if "edit_settings" not in st.session_state:
+        st.session_state.edit_settings = copy.deepcopy(active_run.settings)
+
+    settings = st.session_state.edit_settings
+
+    col1, col2, col3, col4, col5 = st.columns(5, vertical_alignment="bottom")
 
     # text input for editing the current run name
     new_name = col1.text_input(
@@ -189,11 +202,24 @@ elif st.session_state.main_mode == 1:
         st.session_state.all_runs_key = new_name
         st.rerun()
     
-    if col2.button("Close"):
+    if col2.button("Save"):
+        del(st.session_state.all_runs[st.session_state.all_runs_key])
+        st.session_state.all_runs[st.session_state.all_runs_key] = MixedLayerModel(st.session_state.edit_settings)
+        st.rerun()
+
+    if col3.button("Save & close"):
+        del(st.session_state.all_runs[st.session_state.all_runs_key])
+        st.session_state.all_runs[st.session_state.all_runs_key] = MixedLayerModel(st.session_state.edit_settings)
         st.session_state.main_mode = 0
         st.rerun()
 
-    active_run = st.session_state.all_runs[st.session_state.all_runs_key]
+    if col4.button("Reset"):
+        active_run = st.session_state.all_runs[st.session_state.all_runs_key]
+        st.rerun()
+
+    if col5.button("Close"):
+        st.session_state.main_mode = 0
+        st.rerun()
     
     tab_default, tab_fire = st.tabs(["Default", "Fire plume"])
     
@@ -201,7 +227,7 @@ elif st.session_state.main_mode == 1:
         col1, col2 = st.columns(2)
         with col1:
             with st.expander("General", expanded=True):
-                runtime = st.number_input(
+                settings["runtime"] = st.number_input(
                     r"runtime (s)",
                     help="total runtime (s)",
                     value=active_run.settings["runtime"],
@@ -209,77 +235,78 @@ elif st.session_state.main_mode == 1:
                     format="%0.0f",
                 )
     
-                dt = st.number_input(
+                settings["dt"] = st.number_input(
                     r"$\Delta t$ (s)",
                     help="time step (s)",
-                    value=default_settings["dt"],
+                    value=active_run.settings["dt"],
                     step=0.1,
                     format="%0.1f",
                 )
     
-                dt_output = st.number_input(
+                settings["dt_output"] = st.number_input(
                     r"output $\Delta t$ (s)",
                     help="output time step (s)",
-                    value=default_settings["dt_output"],
+                    value=active_run.settings["dt_output"],
                     step=0.1,
                     format="%0.1f",
                 )
     
             with st.expander("Mixed layer", expanded=True):
-                h = st.number_input(
+                settings["h"] = st.number_input(
                     r"$h$ (m)",
                     help="boundary-layer depth (m)",
-                    value=default_settings["h"],
+                    value=active_run.settings["h"],
                     step=1.0,
                     format="%0.0f",
                 )
     
-                beta = st.number_input(
+                settings["beta"] = st.number_input(
                     r"$\beta$ (-)",
                     help="entrainment coefficient (-)",
-                    value=default_settings["beta"],
+                    value=active_run.settings["beta"],
                     step=0.01,
                     format="%0.2f",
                 )
     
-                div = st.number_input(
-                    r"$\div$ (s-1)",
+                settings["div"] = st.number_input(
+                    r"$div$ (s-1)",
                     help="large-scale divergence (s-1)",
-                    value=default_settings["div"],
+                    value=active_run.settings["div"],
                     step=0.000001,
-                    format="%0.3f",
+                    format="%0.3e",
                 )
     
         with col2:
             with st.expander("Temperature", expanded=True):
-                theta = st.number_input(
+                settings["theta"] = st.number_input(
                     r"$\theta$ (K)",
                     help="mixed-layer potential temperature (K)",
-                    value=default_settings["theta"],
+                    value=active_run.settings["theta"],
                     step=0.1,
                     format="%0.1f",
                 )
     
-                dtheta = st.number_input(
+                settings["dtheta"] = st.number_input(
                     r"$\Delta \theta$ (K)",
                     help="potential temperature jump (K)",
-                    value=default_settings["dtheta"],
+                    value=active_run.settings["dtheta"],
                     step=0.01,
                     format="%0.2f",
                 )
     
-                wtheta = st.number_input(
+                settings["wtheta"] = st.number_input(
                     r"$\overline{w^\prime \theta^\prime}_s$ (K m s-1)",
                     help="potential temperature surface flux (K m s-1)",
-                    value=default_settings["wtheta"],
+                    value=active_run.settings["wtheta"],
                     step=0.01,
                     format="%0.2f",
                 )
     
-                gamma_theta = st.number_input(
+                settings["gamma_theta"] = st.number_input(
                     r"$\gamma_\theta$ (K m-1)",
                     help="potential temperature lapse rate (K m-1)",
-                    value=default_settings["gammatheta"],
+                    value=active_run.settings["gammatheta"],
                     step=0.0001,
                     format="%0.4f",
                 )
+
