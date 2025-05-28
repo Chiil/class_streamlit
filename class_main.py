629113c960754bb39fee03d121e187816209d481
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2025-05-28 22:42:03 +0200

    Cleanup, still need to switch twice for axes.

--- class_main.py
+++ class_main.py
@@ -172,15 +172,15 @@ with st.sidebar:
         if f"_plot_{i}_runs" not in st.session_state:
             st.session_state[f"_plot_{i}_runs"] = list(st.session_state.all_runs.keys())
 
+        if f"plot_{i}_runs" not in st.session_state:
+            st.session_state[f"plot_{i}_runs"] = st.session_state[f"_plot_{i}_runs"]
+
         with st.container(border=True):
             st.header(f"Plot {i}")
             x_axis, y_axis = st.columns(2)
             plot.xaxis_key = x_axis.selectbox("X-axis", plot.xaxis_options, index=plot.xaxis_index, key=f"plot_{i}_xaxis")
             plot.yaxis_key = y_axis.selectbox("Y-axis", plot.yaxis_options, index=plot.yaxis_index, key=f"plot_{i}_yaxis")
 
-            st.session_state[f"plot_{i}_runs"] = st.session_state[f"_plot_{i}_runs"]
-
-            # plot.selected_runs = st.multiselect(
             plot.selected_runs = st.multiselect(
                 "Runs to plot",
                 options=list(st.session_state.all_runs.keys()),
@@ -190,7 +190,6 @@ with st.sidebar:
             # Store to be saved, the underscore is a workaround for streamlit cleaning up the keys.
             plot.xaxis_index = plot.xaxis_options.index(plot.xaxis_key)
             plot.yaxis_index = plot.yaxis_options.index(plot.yaxis_key)
-            plot.selected_runs = st.session_state[f"plot_{i}_runs"]
             st.session_state[f"_plot_{i}_runs"] = st.session_state[f"plot_{i}_runs"]
 
     # with st.container(border=True):
